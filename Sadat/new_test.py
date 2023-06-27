b9f35d57bcde6ea6a838418ee53cb275c3f95143
Ahnaf Sadat <65472393+ahnafsadat@users.noreply.github.com>
2023-06-26 22:55:51 -0500

    Updated - For a single type of product- complete
    Supply>demand constraint removed,
    Quantity transported based on the price at seller --> included in the objective

--- Sadat/new_test.py
+++ Sadat/new_test.py
@@ -5,39 +5,45 @@ import math
 rnd = np.random
 rnd.seed(15)
 
-nc = 12 # number of counties
+nc = 6 # number of counties
 xc = {i:(rnd.rand(1)-.5)*100 for i in range(0,nc)}
 yc = {i:(rnd.rand(1)-.5)*100 for i in range(0,nc)}
 
 for i in range(0,nc):
     plt.scatter(float(xc[i]),float(yc[i]),c='r',marker='o')
+    plt.annotate(i, (float(xc[i]-2.5),float(yc[i]-2.5)))
 
 
-A = [(i,j) for i in range(nc) for j in range(nc)]
-c = {(i,j):np.hypot(xc[i]-xc[j],yc[i]-yc[j])[0] for i,j in A}
+couple = [(i,j) for i in range(nc) for j in range(nc)]
+single = [i for i in range(nc)]
+
+c = {(i,j):np.hypot(xc[i]-xc[j],yc[i]-yc[j])[0] for i,j in couple}
 
 D = {i:np.around(rnd.rand(1)*1000,2)[0] for i in range(nc)}
-S = {i:np.around(rnd.rand(1)*1200,2)[0] for i in range(nc)}
+S = {i:np.around(rnd.rand(1)*1000,2)[0] for i in range(nc)}
+P = {i:100+np.around(rnd.rand(1)*100,2)[0] for i in range(nc)}
 
-while(sum(S.values())<sum(D.values())):
-    S = {i: np.around(rnd.rand(1) * 1200, 2)[0] for i in range(nc)}
 
 from gurobipy import Model, GRB, quicksum
 
 mdl = Model('LP')
 
-x = mdl.addVars(A, vtype=GRB.CONTINUOUS)
+x = mdl.addVars(couple, vtype=GRB.CONTINUOUS)
+R = mdl.addVars(single, vtype=GRB.CONTINUOUS)
 
-mdl.modelSense = GRB.MINIMIZE
+mdl.modelSense = GRB.MAXIMIZE
 
-mdl.setObjective(quicksum(x[i,j]*c[i,j] for (i,j) in A))
-mdl.addConstrs(quicksum(x[i,j] for i in range(nc)) >= D[j] for j in range(nc))
+mdl.setObjective(quicksum(P[i]*R[i] for i in single) - quicksum(x[i,j]*c[i,j] for (i,j) in couple))
+
+mdl.addConstrs(quicksum(x[i,j] for i in range(nc)) == R[j] for j in range(nc))
 mdl.addConstrs(quicksum(x[i,j] for j in range(nc)) <= S[i] for i in range(nc))
+mdl.addConstrs(R[j]  <= D[j] for j in range(nc))
+
 
 mdl.write('LP.lp')
 mdl.optimize()
 
-active_arcs = [a for a in A if x[a].x > 0]
+active_arcs = [a for a in couple if x[a].x > 0]
 
 for i, j in active_arcs:
     if i !=j:
@@ -50,3 +56,7 @@ plt.show()
 result = np.zeros([nc,nc])
 for (i,j) in x.keys():
     result[i,j] = np.around(x[(i,j)].x,2)
+
+import pandas as pd
+results = pd.DataFrame(result)
+results.to_csv("results.csv")
